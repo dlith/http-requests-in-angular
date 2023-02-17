9907ceea24e0cb26cc41e3703f33f82e6f81bd90
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-17 20:41:09 +0100

    Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  private readonly BASE_URL = 'https://ng-complete-guide-4b80d-default-rtdb.europe-west1.firebasedatabase.app/';
 
   constructor(private http: HttpClient) {}
 
@@ -15,7 +16,10 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http.post(this.BASE_URL + 'posts.json',  postData)
+    .subscribe(responseDate=>{
+      console.log(responseDate);
+    });
   }
 
   onFetchPosts() {
