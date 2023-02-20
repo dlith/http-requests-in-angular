3ca0e7cb9f3d72ec835e0b0ac4140048f640f09c
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-20 18:06:23 +0100

    Setting Headers

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from "rxjs/operators";
@@ -23,7 +23,10 @@ export class PostService {
     }
 
     fetchPosts(){
-      return this.http.get<{[key: string]: Post}>(this.BASE_URL)
+      return this.http.get<{[key: string]: Post}>(this.BASE_URL, 
+        {
+          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+        })
       .pipe(
         map((responseDate)=>{
         const postsArray: Post[] =  [];
