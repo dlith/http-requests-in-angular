5759b8b82a1658d3d7d858ca746138cc732302ad
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2023-02-20 18:10:42 +0100

    Adding Query Params

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Subject, throwError } from "rxjs";
 import { map, catchError } from "rxjs/operators";
@@ -23,9 +23,13 @@ export class PostService {
     }
 
     fetchPosts(){
+      let searchParams = new HttpParams();
+      searchParams = searchParams.append('print', 'pretty');
+      searchParams = searchParams.append('custom', 'key');
       return this.http.get<{[key: string]: Post}>(this.BASE_URL, 
         {
-          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+          headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+          params: searchParams
         })
       .pipe(
         map((responseDate)=>{
